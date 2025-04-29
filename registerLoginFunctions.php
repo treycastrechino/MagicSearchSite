7ca3b817d9ab97223a1ea2efc28221e9e5c6499b
treycastrechino <treycastrechino@gmail.com>
2025-04-29 09:54:10 -0400

    Cleaned up the register login code pages and moved all functions to a separate page for readability.

---
+++ registerLoginFunctions.php
@@ -0,0 +1,180 @@
+<?php
+
+function setActiveForm(){
+
+    if (isset($_SESSION['activeForm'])){
+
+
+    }
+    else{
+    
+        $_SESSION['activeForm'] = 'login-form';
+    }
+}
+
+function isActiveForm($pformName, $pactiveForm){
+
+    if ($pformName == $pactiveForm){
+
+        return 'active';
+
+    }
+    else{
+        
+        return '';
+    }
+}
+
+function showError($error){
+
+    if(!empty($error)){
+
+        return "<p class ='error-message'>$error</p>";
+    }
+    else{
+        return '';
+    }
+}
+
+function testUserName($name){
+
+    if(filter_var($name,FILTER_SANITIZE_SPECIAL_CHARS) == $name){
+
+        //name is okay. Continue
+
+    }
+    else{
+
+        $_SESSION['registerError'] = 'Invalid Username';
+        header("Location: Login.php");
+        exit();
+
+    }
+
+
+}
+
+function testEmail($email){
+
+
+    if(filter_var($email,FILTER_VALIDATE_EMAIL) == $email){
+
+        //email is okay. Continue
+
+    }
+    else{
+
+        $_SESSION['registerError'] = 'Invalid Email address';
+        header("Location: Login.php");
+        exit();
+
+    }
+
+
+}
+
+function testPassword($password){
+
+
+    if(strlen($password) >= 8 ){
+
+        //password is okay. Continue
+        
+
+    }
+    else{
+
+        $_SESSION['registerError'] = 'Password must be at least 8 characters';
+        header("Location: Login.php");
+        exit();
+
+        echo "this wasn't supposed to run";
+
+    }
+
+    function setPasswordToHash($password){
+
+        $passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        return $passwordHash;
+    }
+}
+
+function checkUsernameExists($queryResults){
+
+    if($queryResults->num_rows > 0){
+
+        $_SESSION['registerError'] = 'Username is already in use';
+        header("Location: Login.php");
+        exit();
+    }
+    else{
+
+    }
+
+}
+
+function checkEmailExists($queryResults){
+
+    if($queryResults->num_rows > 0){
+    
+        $_SESSION['registerError'] = 'Email is already in use';
+        header("Location: Login.php");
+        exit();
+    }
+    else{
+
+    }
+
+}
+
+function addUserToDatabase($connectionVariable,$name, $passwordHash, $email){
+
+
+    $insertUserQuery = "INSERT INTO user (username, password, email)
+    VALUES   ('$name', '$passwordHash', '$email')";
+    $connectionVariable->query($insertUserQuery);
+
+}
+
+function loginEmailPasswordCheck($queryResults, $password){
+
+    if($queryResults->num_rows > 0){
+
+        $user = $queryResults->fetch_assoc();
+        if(password_verify($password, $user['password'])){
+
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['isLoggedIn'] = true;
+            header("Location: index.php");
+            exit();
+        }
+        else{
+
+            $_SESSION['loginError'] = 'Incorrect Password';
+            header("Location: Login.php");
+            exit();
+        }
+    }
+    else{
+
+        $_SESSION['loginError'] = 'Email not found';
+        header("Location: Login.php");
+        exit();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- register_login_logic.php
+++ register_login_logic.php
@@ -1,39 +1,12 @@
 <?php
 
-if (isset($_SESSION['activeForm'])){
 
+include('registerLoginFunctions.php');
 
-}
-else{
+setActiveForm();
 
-    $_SESSION['activeForm'] = 'login-form';
-}
 
 $activeForm = $_SESSION['activeForm'];
-
-    function isActiveForm($pformName, $pactiveForm){
-
-        if ($pformName == $pactiveForm){
-
-            return 'active';
-
-        }
-        else{
-            
-            return '';
-        }
-    }
-
-    function showError($error){
-
-        if(!empty($error)){
-
-            return "<p class ='error-message'>$error</p>";
-        }
-        else{
-            return '';
-        }
-    }
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -46,79 +19,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $email = $_POST['email'];
         $password = $_POST['password'];
         
-
-        if(filter_var($name,FILTER_SANITIZE_SPECIAL_CHARS) == $name){
-
-            //name is okay. Continue
-
-        }
-        else{
-
-            $_SESSION['registerError'] = 'Invalid Username';
-            header("Location: Login.php");
-            exit();
-
-            echo "this wasn't supposed to run";
-
-        }
-
-        if(filter_var($email,FILTER_VALIDATE_EMAIL) == $email){
-
-            //email is okay. Continue
-
-        }
-        else{
-
-            $_SESSION['registerError'] = 'Invalid Email address';
-            header("Location: Login.php");
-            exit();
-
-            echo "this wasn't supposed to run";
-
-        }
-
-        if(strlen($password) >= 8 ){
-
-            //password is okay. Continue
-            $passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-        }
-        else{
-
-            $_SESSION['registerError'] = 'Password must be at least 8 characters';
-            header("Location: Login.php");
-            exit();
-
-            echo "this wasn't supposed to run";
-
-        }
+        testUserName($name);
+        testEmail($email);
+        testPassword($password);
+        $passwordHash = setPasswordToHash($password);
 
         openConnection();
+
         $checkEmail = $connectionVariable->query("SELECT email FROM user WHERE email = '$email'");
         $checkUsername = $connectionVariable->query("SELECT username FROM user WHERE username = '$name'");
-        if($checkUsername->num_rows > 0){
 
-            $_SESSION['registerError'] = 'Username is already in use';
-            header("Location: Login.php");
-            exit();
-        }
-        else{
-
-        }
-
-        if($checkEmail->num_rows > 0){
-    
-            $_SESSION['registerError'] = 'Email is already in use';
-            header("Location: Login.php");
-            exit();
-        }
-        else{
-
-        }
-
-        $insertUserQuery = "INSERT INTO user (username, password, email)
-                            VALUES   ('$name', '$passwordHash', '$email')";
-        $connectionVariable->query($insertUserQuery);
+        checkUsernameExists($checkUsername);
+        checkEmailExists($checkEmail);
+        addUserToDatabase($connectionVariable, $name, $passwordHash, $email);
 
         $_SESSION['username'] = $name;
         $_SESSION['email'] = $email;
@@ -139,19 +52,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
 
         openConnection();
+
         $checkEmail = $connectionVariable->query("SELECT * FROM user WHERE email = '$email'");
-        if($checkEmail->num_rows > 0){
-    
-            $user = $checkEmail->fetch_assoc();
-            if(password_verify($password, $user['password'])){
-    
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['email'] = $user['email'];
-                $_SESSION['isLoggedIn'] = true;
-                header("Location: index.php");
-                exit();
-            }
-        }
+        loginEmailPasswordCheck($checkEmail,$password);
         
     }
 
